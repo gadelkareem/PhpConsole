355f84e450f53898bbb0c23beef24ed3982791ef
Waleed Gadelkareem <gadelkareem@gmail.com>
2018-06-18 15:50:37 +0000

    Apply fixes from StyleCI

--- Calculate.php
+++ Calculate.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-require_once dirname(__FILE__) . '/Command/AbstractCommand.php';
-require_once dirname(__FILE__) . '/Math/Calculator.php';
+require_once dirname(__FILE__).'/Command/AbstractCommand.php';
+require_once dirname(__FILE__).'/Math/Calculator.php';
 
 new \PhpConsole\Math\Calculator(isset($argv) ? $argv : null);
